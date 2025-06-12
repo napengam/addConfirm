eef9379078b21952ca8574da1bc2676b7701d458
napengam <hgs@hgsweb.de>
2025-06-12 16:31:47 +0200

    Update addConfirm.js

--- addConfirm.js
+++ addConfirm.js
@@ -1,123 +1,73 @@
-function addConfirm(other = null) {
-    //
-    // any clickable objects cb inside document instrumented 
-    // with dummy class 'need-confirm' or other class if given
-    //
-    var cb;
-    if (other === null) {
-        cb = document.querySelectorAll('.need-confirm');
-    } else {
-        cb = document.querySelectorAll('.need-confirm,' + other);
-    }
-    cb.forEach(elem => {
-        // 
-        // as we don't know if the element has allready any
-        // event listeners we clone the element, to get rid of them.
-        // We keep a referenc to the original element and make this one
-        // disapear and replace the origin with the clone  instead.
-        // 
-        if (typeof elem.clonedFromOrigin !== 'undefined') {
-            return; // avoid cloning a clone
+
+function addConfirm(selector = '.need-confirm') {
+    let elements = document.querySelectorAll(selector);
+    let currentTarget = null, dialog, yesBtn, noBtn;
+
+    elements.forEach(el => {
+        if (el.dataset.confirmBound === 'true') {
+            return;
         }
-        let clone = elem.cloneNode(true);
-        clone.elemOnclick = elem.onclick;
-        elem.onclick = '';
-        clone.onclick='';
-        clone.clonedFromOrigin = elem;
-        clone.clonedFromOriginParent = elem.parentNode;
-        elem.replaceWith(clone);
-        clone.clonedFromOrigin.style.display = 'none';
-        // 
-        // we now apply the confirm dialog to the clone
-        //
-        clone.removeEventListener('click', confirmIfClicked);
-        clone.addEventListener('click', confirmIfClicked);
-        // *****************************************
-        // while we loose all eventListeners when cloning
-        // the node, we can delegate events to the original
-        // node using the function below
-        // ******************************************
-        delegateOtherEvents(clone);
+        el.dataset.confirmBound = 'true';
+        el.addEventListener('click', function (e) {
+
+            if (e.target.dataset.confirmBound === 'false') {
+                return;
+            }
+            e.target.dataset.confirmBound = 'true';
+            // Intercept click
+            e.preventDefault();
+            e.stopPropagation();
+
+            // Set current target for confirm dialog
+            currentTarget = el;
+
+
+            openDialog();
+            
+        }, true); // capture = true
     });
-
-    function confirmIfClicked(e) {
-
-
-        // stop any further procesing
-
-        e.stopPropagation();
-        e.preventDefault();
-
+    
+    function openDialog() {
         // *****************************************
-        // open dialog and call it with the current 
-        // value of 'this' that will point here 
-        // to the clone of the original node 
+        // you might need to adapt this to the dialogs
+        // you are using. This is just an example. 
         // ******************************************
 
-        callYourDialog.call(this);
-
-
-    }
-    function callYourDialog() {
-        // *****************************************
-        // As an example:
-        // open simple dialog provided by the calling
-        // page.
-        // You probably have to adapt this function to
-        // work with the dialog of your choice
-        // The value of this should point to the clone
-        // of the original element, else yes() or no() can't
-        // find the event target. 
-        // ******************************************
-
-        let diag = document.getElementById('confirm');
-        diag.querySelector('#cyes').onclick = yes.bind(this);
-        diag.querySelector('#cno').onclick = no.bind(this);
-        diag.showModal();
-
-        function yes() { // YES
-            diag.close();
-            //*****
-            // press this button again
-            // we clicked on a clone so redirect the click to the
-            // original element to execute any event listeners now.
-            //*****
-            let fireOnThis = this.clonedFromOrigin;
-            fireOnThis.onclick = this.elemOnclick;
-            //*****
-            // give original node  back to parent
-            //*****
-            this.clonedFromOriginParent.appendChild(fireOnThis);
-            // *****************************************
-            // stop clone from asking have original node
-            // handle the onclick event
-            // ******************************************
-            this.removeEventListener('click', confirmIfClicked);
-            fireEvent(fireOnThis, 'click');
-            // *****************************************
-            // enable clone again for event 'onclick'
-            // ******************************************
-            this.addEventListener('click', confirmIfClicked);
-        }
-        function no() {
-            diag.close();
-
-        }
+        dialog = document.getElementById('confirm');
+        dialog.showModal();
+        yesBtn = dialog.querySelector('#cyes');
+        noBtn = dialog.querySelector('#cno');
+        yesBtn.onclick = yes;
+        noBtn.onclick = no;
     }
 
-    function delegateOtherEvents(clone) {
-        // *****************************************
-        // all events in the array are delegated to the
-        // original node.  
-        // ******************************************
-        ['mouseover', 'mouseout'].forEach((evt) => {
-            clone.addEventListener(evt, () => {
-                fireEvent(clone.clonedFromOrigin, evt);
-            }, true);
+    function yes() {
+        yesBtn.onclick = noBtn.onclick = null;
+        if (!currentTarget)
+            return;
+
+        // Temporarily unbind interceptor to avoid recursion
+        currentTarget.dataset.confirmBound = 'false';
+
+        // Redispatch original click
+        const event = new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
         });
+        currentTarget.dispatchEvent(event);
 
+        // Rebind after dispatch
+        currentTarget.dataset.confirmBound = 'true';
+        dialog.close();
+        currentTarget = null;
     }
-    function fireEvent(fireOnThis, eventType) {
-        fireOnThis.dispatchEvent(new MouseEvent(eventType, window.event));
-}
+    ;
+
+    function no() {
+        yesBtn.onclick = noBtn.onclick = null;
+        dialog.close();
+        currentTarget = null;
+    }
+    ;
 }
